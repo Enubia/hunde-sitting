785fb1c000ee024b18ac72aa06e53b16b5f96b6e
Enubia <44695100+Enubia@users.noreply.github.com>
2025-03-08 11:14:52 +0100

    refactor: remove unused React ESLint configuration and clean up package dependencies

--- packages/eslint-config/create-config.js
+++ packages/eslint-config/create-config.js
@@ -4,7 +4,6 @@ export default function createConfig(options, ...userConfigs) {
     return antfu(
         {
             type: 'app',
-            typescript: true,
             formatters: true,
             stylistic: {
                 indent: 4,
