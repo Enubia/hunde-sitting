9786a6d7ad4214cb02e08992bec562f01d122088
Enubia <44695100+Enubia@users.noreply.github.com>
2025-04-07 08:50:00 +0200

    adonis setup added

---
+++ adonis/app/models/user.ts
@@ -0,0 +1,31 @@
+import type { DateTime } from 'luxon';
+
+import { withAuthFinder } from '@adonisjs/auth/mixins/lucid';
+import { compose } from '@adonisjs/core/helpers';
+import hash from '@adonisjs/core/services/hash';
+import { BaseModel, column } from '@adonisjs/lucid/orm';
+
+const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
+    uids: ['email'],
+    passwordColumnName: 'password',
+});
+
+export default class User extends compose(BaseModel, AuthFinder) {
+    @column.dateTime({ autoCreate: true })
+    declare createdAt: DateTime;
+
+    @column()
+    declare email: string;
+
+    @column()
+    declare fullName: string | null;
+
+    @column({ isPrimary: true })
+    declare id: number;
+
+    @column({ serializeAs: null })
+    declare password: string;
+
+    @column.dateTime({ autoCreate: true, autoUpdate: true })
+    declare updatedAt: DateTime | null;
+}
---
+++ adonis/eslint.config.js
@@ -0,0 +1,92 @@
+import adonisJSPlugin from '@adonisjs/eslint-plugin';
+import antfu from '@antfu/eslint-config';
+
+export default antfu(
+    {
+        formatters: true,
+        stylistic: {
+            indent: 4,
+            quotes: 'single',
+            semi: true,
+        },
+        type: 'app',
+        yaml: false,
+    },
+    {
+        rules: {
+            'antfu/no-top-level-await': ['off'],
+            curly: ['error', 'all'],
+            'import/order': 'off',
+            'no-console': ['warn', { allow: ['warn', 'error'] }],
+            'node/no-process-env': ['error'],
+            'node/prefer-global/process': 'off',
+            'perfectionist/sort-imports': ['error', {
+                ignoreCase: true,
+                newlinesBetween: 'always',
+                order: 'asc',
+                tsconfigRootDir: '.',
+                type: 'alphabetical',
+                groups: [
+                    'external-type',
+                    'builtin-type',
+                    'internal-type',
+                    'parent-type',
+                    'sibling-type',
+                    'index-type',
+                    'external',
+                    'builtin',
+                    'internal',
+                    ['parent', 'sibling'],
+                    'index',
+                ],
+                internalPattern: ['^#'],
+            }],
+            'perfectionist/sort-classes': ['error', {
+                ignoreCase: true,
+                order: 'asc',
+                type: 'alphabetical',
+                groups: [
+                    'index-signature',
+                    'static-property',
+                    'static-block',
+                    ['protected-property', 'protected-accessor-property'],
+                    ['private-property', 'private-accessor-property'],
+                    ['property', 'accessor-property'],
+                    'constructor',
+                    'static-method',
+                    'protected-method',
+                    'private-method',
+                    'method',
+                    ['get-method', 'set-method'],
+                    'unknown',
+                ],
+            }],
+            'style/brace-style': ['error', '1tbs'],
+            'style/comma-dangle': ['error', 'always-multiline'],
+            'style/eol-last': ['error', 'always'],
+            'style/linebreak-style': ['error', 'unix'],
+            'style/quote-props': ['error', 'as-needed'],
+            'style/quotes': ['error', 'single', { avoidEscape: true }],
+            'ts/consistent-type-definitions': 'off',
+            'unused-imports/no-unused-imports': 'warn',
+        },
+    },
+    {
+        name: 'Client side scripts',
+        files: ['resources/js/**/*.js'],
+        rules: {
+            'no-console': ['error', { allow: ['debug', 'warn', 'error'] }],
+        },
+    },
+    {
+        name: 'AdonisJS',
+        files: ['*.ts'],
+        plugins: {
+            '@adonisjs': adonisJSPlugin,
+        },
+        rules: {
+            '@adonisjs/prefer-lazy-controller-import': ['error'],
+            '@adonisjs/prefer-lazy-listener-import': ['error'],
+        },
+    },
+);
